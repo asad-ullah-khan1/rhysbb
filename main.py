d889d67221f26fb9f759eb2a9d004765f33a5a13
asad-ullah-khan1 <asadkhan602@gmail.com>
2023-09-26 11:11:31 +0500

    eleven

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import streamlit as st
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 
-
 def get_available_files(folder_path):
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
     return [os.path.join(folder_path, f) for f in files]
@@ -39,25 +38,30 @@ def main():
 
         if selected_files:
             for selected_file in selected_files:
-                # st.subheader(f"Viewing {os.path.basename(selected_file)}")
-
                 openai_api_key = st.secrets["OPENAI_API_KEY"]
                 agent = create_csv_agent(OpenAI(temperature=0), selected_file, verbose=True, openai_api_key=openai_api_key)
 
-                # Generate a unique key for the text input widget
-                widget_key = f"userQuestions_{selected_file}"
+                # Initialize conversation history
+                conversation_history = []
 
-            # Provide user instructions or messages
-            st.write("You can ask questions about the CSV data in the text box below.")
-            user_question = st.text_input("Ask a question about your CSV:", key=widget_key)
+                # Provide user instructions or messages
+                st.write("You can ask questions about the CSV data in the text box below.")
+                user_question = st.text_input("Ask a question about your CSV:")
 
-            if user_question:
-                with st.spinner(text="In progress..."):
-                    try:
-                        response = agent.run(user_question)
-                        st.write(response)
-                    except Exception as e:
-                        st.error(f"An error occurred: {str(e)}")
+                if user_question:
+                    # Append user's question to conversation history
+                    conversation_history.append(f"User: {user_question}")
+
+                    with st.spinner(text="In progress..."):
+                        try:
+                            # Get chatbot's response
+                            response = agent.run("\n".join(conversation_history))
+                            conversation_history.append(f"Chatbot: {response}")
+
+                            # Display the conversation history
+                            st.write("\n\n".join(conversation_history))
+                        except Exception as e:
+                            st.error(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     main()
